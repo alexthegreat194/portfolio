d884fe44496c05aeb82dbffb764d15635cfa4e3a
alexthegreat194 <alexander.gentry@aol.com>
2021-12-28 19:40:51 -0800

    started first section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "pose": "url('/Assets/Background')"
+      },
+    },
+  },
+  plugins: [],
+  height: [],
+}
