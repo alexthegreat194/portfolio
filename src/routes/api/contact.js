c34fa58badb5f125762126aa526959b3594ffbb2
alexthegreat194 <alexander.gentry@aol.com>
2022-02-17 22:18:48 -0800

    works with mangodb

--- src/routes/api/contact.js
+++ src/routes/api/contact.js
@@ -1,16 +1,27 @@
-
+import clientPromise from "$lib/mongodb-client";
 
 export const post = async ({ request }) => {
+    // database connection
+    const connection = await clientPromise;
+    const db = connection.db()
+    const collection = db.collection('emails')
+
+    // form parsing
     const body = await request.formData();
     const email = body.get("email");
     const subject = body.get("subject");
     const message = body.get("message");
+
     const data = { email, subject, message }
-  
+    console.log(data)
+
+    const newEmail = await collection.insertOne(data)
+    console.log(newEmail);
+
     return {
         status: 300,
         headers: {
             location: '/'
         }
     };
-  };
+};
