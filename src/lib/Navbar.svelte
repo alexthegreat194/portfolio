d3d8dbfb7e030fb325434a9f650b6497e8dfc246
alexthegreat194 <alexander.gentry@aol.com>
2022-03-09 00:23:55 -0800

    adjusted nav

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -4,14 +4,17 @@
 
 <!-- Navbar -->
 <div class="h-14 w-screen fixed z-10 top-0 bg-gray-900 text-gray-50
-flex justify-between items-center
-px-10">
-    <div class="h-full flex items-center font-bold text-2xl text-red-700">
+grid grid-cols-2">
+    <div class="h-full 
+        flex items-center justify-center gap-3
+        font-bold text-xl sm:text-2xl 
+        text-red-700">
         <img src="{logo}" alt="Logo" class="h-3/4">
-        <span class="px-5">Alex Harlan</span>
+        <span class="whitespace-nowrap">Alex Harlan</span>
     </div>
-    <nav class="font-light text-xl text-gray-50">
-        <a href="#projects" class="px-10">Projects</a>
-        <a href="#contact" class="px-10">Contact</a>
+    <nav class="font-light text-md sm:text-xl text-gray-50
+        flex items-center justify-center gap-6">
+        <a href="#projects" class="">Projects</a>
+        <a href="#contact" class="">Contact</a>
     </nav>
 </div>
