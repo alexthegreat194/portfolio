12f9bc73f6189dd70a8e54f31d1ca22b3e9aa26a
alexthegreat194 <alexander.gentry@aol.com>
2022-03-20 01:24:12 -0700

    made form responsive

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -4,41 +4,44 @@ flex flex-col items-center gap-7
 m-25 mb-32"
 id="contact">
 
-<div class="flex flex-col items-center">
-    <h1 class="text-red-700 text-5xl font-bold">Contact me</h1>
-    <h3 class="text-2xl font-light">I'll get back to you as soon as I can</h3>
-</div>
-
-<form action="/api/contact" method="post" class="
-    w-3/5
-    flex flex-col gap-7">
-
-    <div class="grid grid-cols-12 gap-7">
-        <div class="flex flex-col col-span-5 gap-2">
-            <label for="email" class="font-extralight">Email</label>
-            <input type="email" name="email" id="email" class="
-            text-gray-900 bg-gray-100 text-2xl p-2 custom-shadow">
-        </div>
-
-        <div class="flex flex-col col-span-7 gap-2">
-            <label for="subject" class="font-extralight">Subject</label>
-            <input type="text" name="subject" id="subject" class="
-            text-gray-900 bg-gray-100 text-2xl p-2 custom-shadow">
-        </div>
+    <div class="flex flex-col items-center">
+        <h1 class="text-red-700 text-4xl sm:text-5xl font-bold">Contact me</h1>
+        <h3 class="text-xl sm:text-2xl font-light">I'll get back to you as soon as I can</h3>
     </div>
 
-    <div class="flex flex-col gap-2">
-        <label for="message" class="font-extralight">Message</label>
-        <textarea id="message" name="message" rows="4" cols="50" class="
-        text-gray-900 bg-gray-100 text-2xl p-2 custom-shadow"></textarea>
-    </div>
+    <form action="/api/contact" method="post" class="
+        w-3/5
+        flex flex-col gap-7">
 
-    <input type="submit" value="Submit" class="
-    text-3xl font-medium px-7 py-3 mx-2 rounded-full
-    border border-red-700
-    bg-red-700 
-    hover:bg-gray-900 hover:text-red-700 hover:cursor-pointer transition-colors">
+        <div class="grid gap-7 
+            grid-cols-1 sm:grid-cols-12
+            grid-rows-2 sm:grid-rows-1">
 
-</form>
+            <div class="flex flex-col col-span-1 sm:col-span-5 gap-1 w-full">
+                <label for="email" class="font-extralight">Email</label>
+                <input type="email" name="email" id="email" class="
+                text-gray-900 bg-gray-100 text-xl p-2 custom-shadow">
+            </div>
+
+            <div class="flex flex-col col-span-1 sm:col-span-7 gap-1 w-full">
+                <label for="subject" class="font-extralight">Subject</label>
+                <input type="text" name="subject" id="subject" class="
+                text-gray-900 bg-gray-100 text-xl p-2 custom-shadow">
+            </div>
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <label for="message" class="font-extralight">Message</label>
+            <textarea id="message" name="message" rows="4" cols="50" class="
+            text-gray-900 bg-gray-100 text-lg p-2 custom-shadow"></textarea>
+        </div>
+
+        <input type="submit" value="Submit" class="
+        text-3xl font-medium px-7 py-3 mx-2 rounded-full
+        border border-red-700
+        bg-red-700 
+        hover:bg-gray-900 hover:text-red-700 hover:cursor-pointer transition-colors">
+
+    </form>
 
 </div>
