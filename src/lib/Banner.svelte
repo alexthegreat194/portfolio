23f3376926f8b7c3eaaafd548ac3ae227763bb98
alexthegreat194 <alexander.gentry@aol.com>
2022-04-16 18:07:21 -0700

    changed portrait photos

--- src/lib/Banner.svelte
+++ src/lib/Banner.svelte
@@ -1,21 +1,23 @@
 <script>
     import background from '$lib/assets/Background-transperent.png'
+    import background2 from '$lib/assets/Background2-transparent.png'
     import profile from '$lib/assets/Profile.jpg'
+    import profile2 from '$lib/assets/Profile2.jpg'
 </script>
 
 
 <!-- Banner -->
 <div class="
-h-screen w-screen flex justify-center items-center
-bg-pose bg-no-repeat bg-cover bg-center bg-blend-overlay bg-opacity-75 bg-fixed"
-style="background-image: url('{background}');"
-id="banner">
+    h-screen w-screen flex justify-center items-center
+    bg-pose bg-no-repeat bg-cover bg-center bg-blend-overlay bg-opacity-75 bg-fixed"
+    style="background-image: url('{background2}');"
+    id="banner">
     <div class="flex flex-col items-center gap-1 sm:gap-3" >
         <h1 class="text-center text-gray-50 font-bold text-3xl sm:text-6xl" >Hi, I'm <span class="text-red-700">Alex Harlan</span></h1>
         <h5 class="text-center text-gray-50 font-extralight text-md sm:text-xl" >Backend Web Developer</h5>
 
         <img class="object-cover w-[200px] h-[200px] rounded-full 
         border-8 border-solid border-gray-900"  
-        src="{profile}" alt="profile">
+        src="{profile2}" alt="profile">
     </div>
 </div>
