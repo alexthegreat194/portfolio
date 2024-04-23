c9326714a447e3786411105bde6d74fa64fd7667
alexthegreat194 <alexander.gentry@aol.com>
2024-04-22 19:54:38 -0700

    links in contacts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,8 @@
     import Navbar from "$lib/Navbar.svelte";
     import Banner from "$lib/Banner.svelte";
     import Projects from "$lib/Projects.svelte";
-    import Form from "$lib/Form.svelte";
     import spacer from '$lib/assets/layered-waves-haikei.svg'
+	import Contact from "$lib/Contact.svelte";
 </script>
 
 <Navbar />
@@ -11,4 +11,4 @@
 <div id="projects" class="spacer rotate-180 flex" style="background-image: url('{spacer}');"></div>
 <Projects />
 <div class="spacer flex" style="background-image: url('{spacer}');"></div>
-<Form />
+<Contact />
